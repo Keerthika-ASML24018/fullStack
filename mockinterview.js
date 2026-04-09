4757ea6f4db578c2ec90a28a4f61e13343e7c91e
Keerthika-ASML24018 <ppkeerthika@gmail.com>
2026-04-09 14:33:59 +0530

    Add files via upload

---
+++ mockinterview.js
@@ -0,0 +1,30 @@
+// Round 2: JavaScript 
+// Q1. What is the difference between var, let, and const? 
+// Q2. What is the difference between == and ===? 
+// Q3. What is DOM and how do you access elements in JavaScript? 
+var a = 10;
+var l = 20; 
+a = 30;     
+console.log(a);
+
+let b = 10;
+b = 30;     
+console.log(b);
+
+const o = 30;
+console.log(o);
+
+var j= 90;
+let j= 48;
+const  j = 9;
+
+if(m==b){
+    console.log()
+}
+console.log(m===b);
+
+
+ <button id="button" onclick="alert()">click me</button>
+ function alert(){
+         onclick=alert("you have clicked me")
+}
